7e79d0091c17eeb89292c7fb55451b9cfa71005d
ddontula3 <111503786+ddontula3@users.noreply.github.com>
2023-03-29 13:54:12 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,14 @@
+class array1{
+public static void main(String args[])throws Exception
+{
+int a[];
+a=new int[3];
+a[0]=3;
+a[1]=5;
+a[2]=8;
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
